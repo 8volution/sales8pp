009998802a01a3333fe282fc728f646404efed74
PreethikaMS <preethika.sd@8volution.com>
2017-01-25 16:15:08 +0800

    Included sales_rep as a new user role

---
+++ app/models/spree/ability_decorator.rb
@@ -0,0 +1,14 @@
+class AbilityDecorator
+  include CanCan::Ability
+  def initialize(user)
+    if user.respond_to?(:has_spree_role?) && user.has_spree_role?('sales_rep')
+      can [:admin, :manage], Spree::Order
+      can [:admin, :index, :show], Spree::Product
+
+    end
+  end
+end
+
+Spree::Ability.register_ability(AbilityDecorator)
+
+
