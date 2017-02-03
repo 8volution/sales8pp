05a6f9356d45a04bfcde3254813968df8dd5f490
PreethikaMS <preethika.sd@8volution.com>
2017-02-03 14:13:29 +0800

    display and save sales rep dropdown in admin user tab

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170123073762) do
+ActiveRecord::Schema.define(version: 20170203054714) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -981,6 +981,7 @@ ActiveRecord::Schema.define(version: 20170123073762) do
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.string   "sales_rep_name"
     t.index ["bill_address_id"], name: "index_spree_users_on_bill_address_id", using: :btree
     t.index ["deleted_at"], name: "index_spree_users_on_deleted_at", using: :btree
     t.index ["email"], name: "email_idx_unique", unique: true, using: :btree
